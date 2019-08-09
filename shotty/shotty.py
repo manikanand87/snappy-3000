e0c394e70c089bc141b7f4ed725a319d599d91d3
manik <manik@CORP.AMAZONWORKSPACES.COM>
2019-08-09 19:42:34 -0400

    throw user exception if instance state error occures

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name = 'shotty')
@@ -129,7 +130,11 @@ def stop_call(project):
 
     for i in instances:
         print("stopping {}".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -144,7 +149,11 @@ def start_call(project):
 
     for i in instances:
         print("starting {}".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
